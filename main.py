cbcd6ee630ff2c887e66ffece6fc126a14c1456a
Jairo Enrique Serrano Castañeda <jairo.serrano@gmail.com>
2024-11-22 01:00:40 +0000

    actualización a plantillas

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+  return render_template("servicios.html", titulo="Inicio y Servicios")
 
 
 @app.route('/ofertas')
 def ofertas():
-  return render_template("ofertas.html")
+  return render_template("ofertas.html", titulo="Ofertas")
 
 
 @app.route('/productos')
 def productos():
-  return render_template("productos.html")
+  return render_template("productos.html", titulo="Productos")
 
 
 @app.route('/equipo')
 def equipo():
-  return render_template("equipo.html")
+  return render_template("equipo.html", titulo="Equipo Humano")
 
 
 @app.route('/contacto')
 def contacto():
-  return render_template("contacto.html")
+  return render_template("contacto.html", titulo="Contáctenos!")
 
 
 if __name__ == "__main__":
