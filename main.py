d96310d5e05b4569291707b6af14aa9a22038a86
Jairo Enrique Serrano Castañeda <jairo.serrano@gmail.com>
2024-11-26 01:55:01 +0000

    Actualizado con validación de formularios

--- main.py
+++ main.py
@@ -1,31 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-  return render_template("servicios.html", titulo="Inicio y Servicios")
+  return render_template("servicios.html",
+                         titulo="Página de Inicio y Servicios")
+
+
+@app.route('/login', methods=['POST'])
+def login_validar():
+  if request.form.get("login") == "admin" and request.form.get(
+      "password") == "123456":
+    return render_template("dashboard.html", titulo="Usuarios autenticados")
+  else:
+    return render_template("login.html", titulo="Usuario no autenticado")
+
+
+@app.route('/login', methods=['GET'])
+def login_formulario():
+  return render_template("login.html", titulo="Ingreso a usuarios")
 
 
 @app.route('/ofertas')
 def ofertas():
-  return render_template("ofertas.html", titulo="Ofertas")
+  return render_template("ofertas.html", titulo="Las mejores Ofertas")
 
 
 @app.route('/productos')
 def productos():
-  return render_template("productos.html", titulo="Productos")
+  return render_template("productos.html", titulo="Productos Destacados")
 
 
 @app.route('/equipo')
 def equipo():
-  return render_template("equipo.html", titulo="Equipo Humano")
+  return render_template("equipo.html", titulo="El mejor Equipo Humano")
 
 
-@app.route('/contacto')
+@app.route('/contacto', methods=['GET'])
 def contacto():
-  return render_template("contacto.html", titulo="Contáctenos!")
+  return render_template("contacto.html", titulo="Contáctenos!!!")
+
+
+@app.route('/contacto', methods=['POST'])
+def guardar_contacto():
+  name = request.form.get("inputName")
+  email = request.form.get("inputEmail")
+  address1 = request.form.get("inputAddress")
+  address2 = request.form.get("inputAddress2")
+  phone = request.form.get("inputPhone")
+  city = request.form.get("inputCity")
+  state = request.form.get("inputState")
+  zip = request.form.get("inputZip")
+  message = request.form.get("inputMessage")
+
+  # Validaciones
+  if name == "" or email == "" or address1 == "" or phone == "" or city == "" or state == "":
+    validacion = "Error: No hay datos para contacto."
+  else:
+    # La información se almacenará en la base de datos
+    validacion = "Gracias por contactarnos, pronto nos pondremos en contacto con usted."
+
+  return render_template("gracias.html",
+                         titulo="Pronto te contactaremos.",
+                         validacion=validacion)
 
 
 if __name__ == "__main__":
